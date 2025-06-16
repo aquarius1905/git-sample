2d2c3e74c7121961af95db5f8080a05e5ab182fc
aquarius1905 <reiko6122@gmail.com>
2025-06-16 14:35:51 +0900

    sample.js再修正

--- sample.js
+++ sample.js
@@ -1,1 +1,4 @@
-console.log(typeof [1, 2, 3]); // "object"
+console.log(typeof 'hello'); // "string"
+console.log(typeof 42);      // "number"
+console.log(typeof true);    // "boolean"
+console.log(typeof undefined); // "undefined"
